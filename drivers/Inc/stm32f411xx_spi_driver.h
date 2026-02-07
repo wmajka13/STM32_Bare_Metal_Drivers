069e421c68b9cd9ba2e6ca6d112b5ea405517d06
Wiktor Majka <wiktormajka13@gmail.com>
2026-02-07 17:11:29 +0100

    Wrote #defines for SPI configuration options

--- drivers/Inc/stm32f411xx_spi_driver.h
+++ drivers/Inc/stm32f411xx_spi_driver.h
@@ -16,13 +16,13 @@
  */
 typedef struct
 {
-	uint8_t SPI_DeviceMode;
-	uint8_t SPI_BusConfig;
-	uint8_t SPI_SclkSpeed;
-	uint8_t SPI_DFF;
-	uint8_t SPI_CPOL;
-	uint8_t SPI_CPHA;
-	uint8_t SPI_SSM;
+	uint8_t SPI_DeviceMode;						// Choose from @SPI_DeviceMode
+	uint8_t SPI_BusConfig;						// Choose from @SPI_BusConfig
+	uint8_t SPI_SclkSpeed;						// Choose from @SPI_SclkSpeed
+	uint8_t SPI_DFF;							// Choose from @SPI_DFF
+	uint8_t SPI_CPOL;							// Choose from @SPI_CPOL
+	uint8_t SPI_CPHA;							// Choose from @SPI_CPHA
+	uint8_t SPI_SSM;							// Choose from @SPI_SSM
 }SPI_Config_t;
 
 
@@ -35,6 +35,62 @@ typedef struct
 	SPI_Config_t	SPIConfig;
 }SPI_Handle_t;
 
+
+/*
+ * @SPI_DeviceMode
+ */
+#define SPI_DEVICE_MODE_MASTER			1
+#define SPI_DEVICE_MODE_SLAVE			0
+
+
+/*
+ * @SPI_BusConfig
+ */
+#define SPI_BUS_CONFIG_FD						0
+#define SPI_BUS_CONFIG_FH						1
+#define SPI_BUS_CONFIG_SIMPLEX_TXONLY			2
+#define SPI_BUS_CONFIG_SIMPLEX_RXONLY			3
+
+
+/* Written in SPI register - SPI control register 1 - BR (Baud rate control) part - chooses the F_PCLK prescaler 2, 4, 8 ... 256
+ * @SPI_SclkSpeed
+ */
+#define SPI_SCLK_SPEED_DIV2						0
+#define SPI_SCLK_SPEED_DIV4						1
+#define SPI_SCLK_SPEED_DIV8						2
+#define SPI_SCLK_SPEED_DIV16					3
+#define SPI_SCLK_SPEED_DIV32					4
+#define SPI_SCLK_SPEED_DIV64					5
+#define SPI_SCLK_SPEED_DIV128					6
+#define SPI_SCLK_SPEED_DIV256					7
+
+
+/*
+ * @SPI_DFF
+ */
+#define SPI_DIFF_8BITS							0
+#define SPI_DIFF_16BITS 						1
+
+
+/*
+ * @SPI_CPOL
+ */
+#define SPI_CPOL_HIGH							1
+#define SPI_CPOL_LOW							0
+
+
+/*
+ * @SPI_CPHA
+ */
+#define SPI_CPHA_HIGH							1
+#define SPI_CPHA_HIGH							0
+
+
+/*
+ * @SPI_SSM
+ */
+#define SPI_SSM_HW								1
+#define SPI_SSM_SW								0
 
 /*****************************************************************************************
  * 								APIs supported by this driver
