65324561514920b173402572f7a6078383b4f35c
Wiktor Majka <wiktormajka13@gmail.com>
2026-02-09 21:41:39 +0100

    Added declaration in API - SSOE bit

--- drivers/Inc/stm32f411xx_spi_driver.h
+++ drivers/Inc/stm32f411xx_spi_driver.h
@@ -141,6 +141,10 @@ void SPI_IRQHandling(SPI_Handle_t *pSPIHandle);			/* When there is an interrupt,
  * 	Other peripheral control APIs
  */
 void SPI_PeripheralControl(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
+uint8_t SPI_GetFlagStatus(SPI_RegDef_t *pSPIx, uint32_t FlagName);
+void SPI_SSIConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
+void SPI_SSOEConfig(SPI_RegDef_t *pSPIx, uint8_t EnOrDi);
+
 
 
 
