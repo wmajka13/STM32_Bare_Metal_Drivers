5c45e606b2d03e4470501363969af84937c32104
Wiktor Majka <wiktormajka13@gmail.com>
2026-02-19 11:50:08 +0100

    added IRQconfiguration, PeripheralControol

--- drivers/Src/stm32f411xx_i2c_driver.c
+++ drivers/Src/stm32f411xx_i2c_driver.c
@@ -62,3 +62,100 @@ void I2C_DeInit(I2C_RegDef_t *pI2Cx) /* Setting registers back to theirs origina
 	}
 }
 
+
+/*
+ * 	IRQ Configuration and ISR handling
+ */
+
+/***********************************************************
+ * @fn			- I2C_IRQInterruptConfig
+ *
+ * @brief 		- Enables/Disables ability to use Interrupt from I2C
+ *
+ * @param[IRQNumber]		- Number of interrupt
+ * @param[EnorDI]			- Enable or Disable macro
+ *
+ * @return		- none
+ *
+ * @note		- none
+ *
+ */
+void I2C_IRQInterruptConfig(uint8_t IRQNumber, uint8_t EnorDI)
+{
+	if(EnorDI == ENABLE)
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ISER0 Reg
+			*NVIC_ISER0 |= ( 1 << IRQNumber );
+
+		} else if(IRQNumber > 31 && IRQNumber < 64)
+		{
+			//program ISER1 Reg
+			*NVIC_ISER1 |= ( 1 << (IRQNumber % 32) );
+
+		}
+		else if(IRQNumber >= 64 && IRQNumber <= 96)
+		{
+			//program ISER2 reg
+			*NVIC_ISER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	} else
+	{
+		if(IRQNumber <= 31)
+		{
+			//program ICER0 Reg
+			*NVIC_ICER0 |= ( 1 << IRQNumber );
+
+		} else if(IRQNumber > 31 && IRQNumber < 64)
+		{
+			//program ICER1 Reg
+			*NVIC_ICER1 |= ( 1 << (IRQNumber % 32) );
+
+		}
+		else if(IRQNumber >= 64 && IRQNumber <= 96)
+		{
+			//program ICER2 reg
+			*NVIC_ICER2 |= ( 1 << (IRQNumber % 64) );
+		}
+	}
+}
+
+/***********************************************************
+ * @fn			- GPIO_IRQPriorityConfig
+ *
+ * @brief 		- allows to set a interrupt priority
+ *
+ * @param[IRQNumber]		- Number of interrupt
+ * @param[IRQPriority]		- Priority of interrupt
+ *
+ * @return		- none
+ *
+ * @note		- none
+ *
+ */
+void I2C_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
+{
+	// 1. finding out IPR reg
+	uint32_t iprx = IRQNumber / 4;
+	uint8_t iprx_byte = IRQNumber % 4;
+
+	uint8_t shift_amount = ( 8 * iprx_byte ) + (8 - NO_PR_BITS_IMPLEMENTED);
+	*(NVIC_PR_BASE_ADDR + iprx) |= ( IRQPriority << shift_amount );
+}
+
+
+/*
+ * 	Other peripheral control APIs
+ */
+void I2C_PeripheralControl(I2C_RegDef_t *pI2Cx, uint8_t EnOrDi)
+{
+	if(EnOrDi == ENABLE)
+	{
+		pI2Cx->CR1 |= (1 << I2C_CR1_PE);
+	} else
+	{
+		pI2Cx->CR1 &= ~(1 << I2C_CR1_PE);
+	}
+
+}a
