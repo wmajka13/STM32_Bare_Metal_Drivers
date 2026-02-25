f9ee5dce53cf69fe056f9fc72e6d4716f246de00
Wiktor Majka <wiktormajka13@gmail.com>
2026-02-25 14:57:28 +0100

    Defined USART Config structure and Handle structure

--- drivers/Inc/stm32f411xx_usart_driver.h
+++ drivers/Inc/stm32f411xx_usart_driver.h
@@ -11,8 +11,31 @@
 
 #include "stm32f411xx.h"
 
+/*
+ * 	Configuration structure for USART
+ */
+typedef struct
+{
+	uint8_t USART_Mode;
+	uint32_t USART_Baud;
+	uint8_t USART_NoOfStopBits;
+	uint8_t USART_WordLength;
+	uint8_t USART_ParityControl;
+	uint8_t USART_HWFlowControl;
+
+}USART_Config_t;
 
 
+/*
+ * 	Handle structure for USARTx peripheral
+ */
+typedef struct
+{
+	USART_RegDef_t *pUSARTx;
+	USART_Config_t USARTConfig
+
+
+}USART_Handle_t;
 
 
 #endif /* INC_STM32F411XX_USART_DRIVER_H_ */
