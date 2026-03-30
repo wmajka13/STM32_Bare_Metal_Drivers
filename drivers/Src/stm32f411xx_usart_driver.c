c27ee318223b168879bfa490688d8d5216811e4e
Wiktor Majka <wiktormajka13@gmail.com>
2026-03-30 13:40:58 +0200

    Feat: Add USART_init definition (without BRR)

--- drivers/Src/stm32f411xx_usart_driver.c
+++ drivers/Src/stm32f411xx_usart_driver.c
@@ -124,3 +124,82 @@ void USART_IRQPriorityConfig(uint8_t IRQNumber, uint32_t IRQPriority)
 	*(NVIC_PR_BASE_ADDR + iprx) |= ( IRQPriority << shift_amount );
 }
 	
+
+/**************************************		Init and De_init		**************************************/
+
+
+void USART_Init(USART_Handle_t *pUSARTHandle)
+{
+	uint32_t tempreg = 0;
+
+/******************************** Configuration of CR1 ******************************************/
+
+	// 1. Enable the Clock for given USART peripheral
+	USART_PeriClockControl(pUSARTHandle->pUSARTx, ENABLE);
+
+	// 2. Enable USART Tx and/or Rx engines according to the USART_Mode configuration item
+	if (pUSARTHandle->USARTConfig.USART_Mode == USART_MODE_ONLY_TX)
+	{
+		tempreg |= (1 << USART_CR1_TE);
+	} else if (pUSARTHandle->USARTConfig.USART_Mode == USART_MODE_ONLY_RX)
+	{
+		tempreg |= (1 << USART_CR1_RE);
+	} else if (pUSARTHandle->USARTConfig.USART_Mode == USART_MODE_TXRX)
+	{
+		tempreg |= (3 << USART_CR1_RE);
+	}
+
+	// 3. Configure the Word length
+	tempreg |= (pUSARTHandle->USARTConfig.USART_WordLength << USART_CR1_M);
+
+	// 4. Configure Parity control (None, Even, or Odd)
+	if (pUSARTHandle->USARTConfig.USART_ParityControl == USART_PARITY_EN_EVEN)
+	{
+		tempreg |= (1 << USART_CR1_PCE);
+		tempreg &= ~(1 << USART_CR1_PS);
+	} else if (pUSARTHandle->USARTConfig.USART_ParityControl == USART_PARITY_EN_ODD)
+	{
+		tempreg |= (1 << USART_CR1_PCE);
+		tempreg |= (1 << USART_CR1_PS);
+	}
+
+	// 5. Program the CR1 register
+	pUSARTHandle->pUSARTx->CR1 = tempreg;
+
+/******************************** Configuration of CR2 ******************************************/
+	
+	tempreg = 0;
+
+	// 1. Configure the number of stop bits inserted during USART frame transmission
+	tempreg |= (pUSARTHandle->USARTConfig.USART_NoOfStopBits << USART_CR2_STOP);
+
+	// 2. Program the CR2 register
+	pUSARTHandle->pUSARTx->CR2 = tempreg;
+
+/******************************** Configuration of CR3 ******************************************/
+	
+	tempreg = 0;
+	
+	// 1. Configure USART hardware flow control (None, CTS, RTS, or CTS+RTS)
+	if (pUSARTHandle->USARTConfig.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS)
+    {
+        tempreg |= (1 << USART_CR3_CTSE);
+    } 
+    else if (pUSARTHandle->USARTConfig.USART_HWFlowControl == USART_HW_FLOW_CTRL_RTS)
+    {
+        tempreg |= (1 << USART_CR3_RTSE);
+    } 
+    else if (pUSARTHandle->USARTConfig.USART_HWFlowControl == USART_HW_FLOW_CTRL_CTS_RTS)
+    {
+        tempreg |= (1 << USART_CR3_CTSE) | (1 << USART_CR3_RTSE);
+    }
+
+	// 2. Program the CR3 register
+	pUSARTHandle->pUSARTx->CR3 = tempreg;
+
+/******************************** Configuration of BRR (Baudrate register) **********************/
+
+	// Implement the code to configure the baud rate
+	// We will cover this in the lecture. No action required here 
+
+}
